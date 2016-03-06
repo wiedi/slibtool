803034870d84448eecf1a15bd0861c4c2f8ab957
midipix <writeonce@midipix.org>
2016-03-06 12:08:09 -0500

    driver: adjust --help to the nature of this utility.

--- src/skin/slbt_skin_default.c
+++ src/skin/slbt_skin_default.c
@@ -5,8 +5,8 @@ const struct argv_option slbt_default_options[] = {
 	{"version",		0,TAG_VERSION,ARGV_OPTARG_NONE,0,0,0,
 				"show version information"},
 
-	{"help",		'h',TAG_HELP,ARGV_OPTARG_OPTIONAL,0,"short|long",0,
-				"show usage information [listing %s options only]"},
+	{"help",		'h',TAG_HELP,ARGV_OPTARG_OPTIONAL,0,0,0,
+				"show usage information"},
 
 	{0,0,0,0,0,0,0,0}
 };
