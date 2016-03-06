070b2af6384f2d18778126d55577430331cf486f
midipix <writeonce@midipix.org>
2016-03-06 00:45:57 -0500

    driver: disassociate -v from --version (reserved for --verbose).

--- src/skin/slbt_skin_default.c
+++ src/skin/slbt_skin_default.c
@@ -2,7 +2,7 @@
 #include "argv/argv.h"
 
 const struct argv_option slbt_default_options[] = {
-	{"version",		'v',TAG_VERSION,ARGV_OPTARG_NONE,0,0,0,
+	{"version",		0,TAG_VERSION,ARGV_OPTARG_NONE,0,0,0,
 				"show version information"},
 
 	{"help",		'h',TAG_HELP,ARGV_OPTARG_OPTIONAL,0,"short|long",0,
