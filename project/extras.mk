daae2d3a4014d5d18c1939bae1e9349ff15f72ae
midipix <writeonce@midipix.org>
2016-04-23 16:00:35 -0400

    project: support static-only and shared-only installed utility via symlinks.

--- project/extras.mk
+++ project/extras.mk
@@ -6,3 +6,17 @@ CFLAGS_MACHINE		:= -DSLBT_MACHINE=\"$(shell $(CC) $(CFLAGS) -dumpmachine)\"
 
 src/driver/slbt_driver_ctx.o:	CFLAGS += $(CFLAGS_MACHINE)
 src/driver/slbt_driver_ctx.lo:	CFLAGS += $(CFLAGS_MACHINE)
+
+install-app-extras:	app
+
+install-app-extras:
+	mkdir -p $(DESTDIR)$(BINDIR)
+
+	rm -f bin/$(PACKAGE)-shared$(OS_APP_SUFFIX).tmp
+	rm -f bin/$(PACKAGE)-static$(OS_APP_SUFFIX).tmp
+
+	ln -s ./$(PACKAGE)$(OS_APP_SUFFIX) bin/$(PACKAGE)-shared$(OS_APP_SUFFIX).tmp
+	ln -s ./$(PACKAGE)$(OS_APP_SUFFIX) bin/$(PACKAGE)-static$(OS_APP_SUFFIX).tmp
+
+	mv bin/$(PACKAGE)-shared$(OS_APP_SUFFIX).tmp $(DESTDIR)$(BINDIR)/$(PACKAGE)-shared$(OS_APP_SUFFIX)
+	mv bin/$(PACKAGE)-static$(OS_APP_SUFFIX).tmp $(DESTDIR)$(BINDIR)/$(PACKAGE)-static$(OS_APP_SUFFIX)
